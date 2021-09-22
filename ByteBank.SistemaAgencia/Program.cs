820a2e21f6a7022099e34ad68536769c6db254e2
Eduardo Teixeira <dunirvana@gmail.com>
2021-09-22 16:34:05 -0300

    Trabalhando com strings e regex

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using ByteBank.Modelos;
 using ByteBank.Modelos.Funcionarios;
@@ -13,8 +14,11 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+
             //TesteHumanize();
-            TesteExtratorValorDeArgumentosURL();
+            //TesteExtratorValorDeArgumentosURL();
+            //TesteString();
+            TesteRegex();
 
             Console.ReadLine();
         }
@@ -38,6 +42,68 @@ namespace ByteBank.SistemaAgencia
             string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
 
             Console.WriteLine(mensagem);
+        }
+
+        static void TesteString()
+        {
+
+            Console.WriteLine("Testando ToLower");
+            string mensagemOrigem = "PALAVRA";
+            string termoBusca = "ra";
+
+            Console.WriteLine(mensagemOrigem.ToLower());
+
+
+            Console.WriteLine("Testando Replace");
+            termoBusca = termoBusca.Replace('r', 'R');
+            Console.WriteLine(termoBusca);
+
+            termoBusca = termoBusca.Replace('a', 'A');
+            Console.WriteLine(termoBusca);
+
+            Console.WriteLine(mensagemOrigem.IndexOf(termoBusca));
+
+
+            Console.WriteLine("Testando o método Remove");
+            string testeRemocao = "primeiraParte&123456789";
+            int indiceEComercial = testeRemocao.IndexOf('&');
+            Console.WriteLine(testeRemocao.Remove(indiceEComercial, 4));
+            Console.ReadLine();
+
+
+
+            Console.WriteLine("Testando o IsNullOrEmpty");
+            string textoVazio = "";
+            string textoNulo = null;
+            string textoQualquer = "kjhfsdjhgsdfjksdf";
+            Console.WriteLine(String.IsNullOrEmpty(textoVazio));
+            Console.WriteLine(String.IsNullOrEmpty(textoNulo));
+            Console.WriteLine(String.IsNullOrEmpty(textoQualquer));
+
+        }
+
+        static void TesteRegex()
+        {
+            // Olá, meu nome é Guilherme e você pode entrar em contato comigo
+            // através do número 8457-4456!
+
+            // Meu nome é Guilherme, me ligue em 4784-4546
+
+            //  "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
+            //  "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
+            //  "[0-9]{4,5}[-][0-9]{4}";
+            //  "[0-9]{4,5}[-]{0,1}[0-9]{4}";
+            //  "[0-9]{4,5}-{0,1}[0-9]{4}";
+            string padrao = "[0-9]{4,5}-?[0-9]{4}";
+
+            // 879.546.120-20
+            // 879546120-20
+
+            string textoDeTeste = "idyufdgfugfjksdhf 99871-5456 sdjkfgsdjgsjgh sfsdjgsdjghsdfj";
+
+            Match resultado = Regex.Match(textoDeTeste, padrao);
+
+            Console.WriteLine(resultado.Value);
         }
     }
 }
